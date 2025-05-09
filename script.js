4672d5071955ff28e38c2bcfd707ab4433c1b64a
mac <mac@MacBook-Pro-mac.local>
2025-05-09 23:01:17 +0300

    some toutchs commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const tabs = document.querySelectorAll('[data-tab-target]');
+    const tabContents = document.querySelectorAll('[data-tab-content]');
+
+    // Set first tab as active on page load
+    document.querySelector('[data-tab-target="#hot-products"]').classList.add('active');
+    document.querySelector('#hot-products').classList.add('active');
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            const target = document.querySelector(tab.dataset.tabTarget);
+            
+            tabContents.forEach(tabContent => {
+                tabContent.classList.remove('active');
+            });
+            
+            tabs.forEach(tab => {
+                tab.classList.remove('active');
+            });
+            
+            tab.classList.add('active');
+            target.classList.add('active');
+        });
+    });
+});
